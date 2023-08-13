03d44ffb5cd7916c97d8bbbedd84c0dddd01e208
Grato-3917 <grato3917@gmail.com>
2023-08-13 13:46:27 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+
+    public static void main(string[] args)
+      {
+        int a=10;
+        int b = 25 ;
+        System.out.println("Sum of two numbers is " + (a+b));
+      }
+}
